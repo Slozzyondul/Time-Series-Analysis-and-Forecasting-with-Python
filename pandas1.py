f78510d768032d9cf710db7bdb400e45e58bb65d
Slozzyondul <solomonondula@gmail.com>
2024-07-23 09:45:26 +0300

    filtering, calling values, calling indices and changing the indexing format with pandas

--- pandas1.py
+++ pandas1.py
@@ -9,3 +9,18 @@ Created on Tue Jul 23 09:17:51 2024
 import pandas as pd
 
 Age = pd.Series([10,20,30,40],index=['age1','age2', 'age3', 'age4'])
+
+#selecting specific age
+Age.age2
+
+#filtering
+
+filtered_age = Age[Age>=20]
+
+#calling values of the series
+Age.values
+
+#calling indies of the series
+Age.index
+
+Age.index = ['a1', 'a2', 'a3', 'a4']
