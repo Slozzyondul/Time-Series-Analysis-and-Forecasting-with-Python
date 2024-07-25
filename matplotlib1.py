1fa60f2a3300e0eb1f38b18199e81d73e7e5ba67
Slozzyondul <solomonondula@gmail.com>
2024-07-25 15:13:13 +0300

    adding sample dataset to play around with

--- matplotlib1.py
+++ matplotlib1.py
@@ -3,8 +3,14 @@
 """
 Created on Thu Jul 25 13:02:00 2024
 
+visualization using Matplotlib
+
 @author: ondul
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-import matplotlib as plt
+year = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
 
+temp = [0.72,0.61,0.65,0.68,0.75,0.90,1.02,0.93,0.85,0.99,1.02]
