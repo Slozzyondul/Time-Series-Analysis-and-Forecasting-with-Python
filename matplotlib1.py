ef9c2083706a90610740bc9cc15c463bff9ce804
Slozzyondul <solomonondula@gmail.com>
2024-07-25 15:31:41 +0300

    ploting the dataset on a line graph and adding standard graph features

--- matplotlib1.py
+++ matplotlib1.py
@@ -14,3 +14,9 @@ import matplotlib.pyplot as plt
 year = [2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020]
 
 temp = [0.72,0.61,0.65,0.68,0.75,0.90,1.02,0.93,0.85,0.99,1.02]
+
+plt.plot(year,temp)
+plt.xlabel("year")
+plt.ylabel("temperature")
+plt.title("sample for learning only", {'fontsize':12, 'horizontalalignment':'left'})
+plt.show() #for rendering the graph
