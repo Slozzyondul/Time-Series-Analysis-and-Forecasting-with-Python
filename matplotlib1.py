496b197f6772b2d27aa7eadb51b5aa47ae84b549
Slozzyondul <solomonondula@gmail.com>
2024-07-25 15:45:27 +0300

    using bivariate dataset this time, setting it up

--- matplotlib1.py
+++ matplotlib1.py
@@ -18,5 +18,5 @@ temp = [0.72,0.61,0.65,0.68,0.75,0.90,1.02,0.93,0.85,0.99,1.02]
 plt.plot(year,temp)
 plt.xlabel("year")
 plt.ylabel("temperature")
-plt.title("sample for learning only", {'fontsize':12, 'horizontalalignment':'left'})
+plt.title("sample for learning only", {'fontsize':12, 'horizontalalignment':'left '})
 plt.show() #for rendering the graph
---
+++ matplotlib2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 25 15:36:38 2024
+
+@author: ondul
+"""
+
+month = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+
+customer1 = [12,13,9,8,7,8,8,7,6,5,8,10]
+
+customer2 = [14,16,11,7,6,6,7,6,5,8,9,12]
