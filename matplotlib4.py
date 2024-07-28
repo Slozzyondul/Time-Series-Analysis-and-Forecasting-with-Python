12eb52a3f10b182e8e957b270c718f7ab4608d27
Slozzyondul <solomonondula@gmail.com>
2024-07-28 15:15:01 +0300

    restyling and generating extra two customer to test its functionality

--- matplotlib4.py
+++ matplotlib4.py
@@ -16,6 +16,9 @@ customer1 = [12,13,9,8,7,8,8,7,6,5,8,10]
 
 customer2 = [14,16,11,7,6,6,7,6,5,8,9,12]
 
+customer3 = np.random.rand(12)
+customer4 = np.random.rand(12)
+
 #generating a bar graph
 plt.bar(month,customer1,color = 'red', label='customer1')
 plt.bar(month,customer2,color = 'yellow', label='customer2')
@@ -35,8 +38,14 @@ plt.bar(month_values, customer1, bar_width, color='blue', label='customer1')
 
 plt.bar(month_values+bar_width, customer2, bar_width, color='black', label='customer2')
 
+plt.bar(month_values+bar_width+bar_width, customer3, bar_width, color='red', label='customer3')
+
+plt.bar(month_values+bar_width+bar_width+bar_width, customer4, bar_width, color='green', label='customer4')
+
 plt.xlabel('month')
 plt.ylabel('consumption') 
 plt.title('bar plot sample graph')
+plt.xticks(month_values + (bar_width)/12, ('jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'))
 plt.legend(loc = 'best')
 plt.show()
+
