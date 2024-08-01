88a3e22caabfdf1b1d8cf9b056bd7069f62a071f
Slozzyondul <solomonondula@gmail.com>
2024-08-01 16:12:50 +0300

    decomposing the dataset to view trend, seasonality and residual graph

--- Statistical_models_for_time_series/ARIMA.py
+++ Statistical_models_for_time_series/ARIMA.py
@@ -12,6 +12,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from statsmodels.tsa.seasonal import seasonal_decompose
+
 df = pd.read_csv('Temp-Data.csv', index_col = 'DATE', parse_dates=True)
 
 df.index.freq = 'D'
@@ -26,3 +28,7 @@ df.plot()
 
 train = df.iloc[:510,0]
 test = df.iloc[510:,0]
+
+
+decomp_result = seasonal_decompose(df)
+decomp_result.plot()
