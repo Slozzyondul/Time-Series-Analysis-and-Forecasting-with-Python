4681434c3441b1ce54b6e5efa437c1fa9f661100
Slozzyondul <solomonondula@gmail.com>
2024-08-01 15:49:51 +0300

    importing dataset to use with ARIMA model

--- Statistical_models_for_time_series/ARIMA.py
+++ Statistical_models_for_time_series/ARIMA.py
@@ -3,6 +3,16 @@
 """
 Created on Tue Jul 30 17:10:24 2024
 
+ARIMA
+
 @author: ondul
 """
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Temp-Data.csv', index_col = 'DATE', parse_dates=True)
+
+df.index.freq = 'D'
+df.dropna(inplace=True)
